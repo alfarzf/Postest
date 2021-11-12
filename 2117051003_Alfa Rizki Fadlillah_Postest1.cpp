3529835bbfe27fdb8c7a777e991ed35421c6104e
Alfa Rizki Fadlillah <92286829+alfarzf@users.noreply.github.com>
2021-11-12 20:43:07 +0700

    Add files via upload

---
+++ 2117051003_Alfa Rizki Fadlillah_Postest1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	/*Alfa Rizki Fadlillah
+		Post Test Kelas C
+		2117051003*/
+	
+	string nama1;
+	string nama2;
+	
+	//input
+	getline(cin,nama1);
+	getline(cin,nama2);
+	
+	//output
+	cout
+	<<"(:|------Percakapan singkat------|:)"<<endl
+	<<nama1<<"\t: Hai salam kenal namaku \""<<nama1<<"\", nama kamu siapa?"<<endl
+	<<nama2<<"\t: Hai salam kenal juga namaku \""<<nama2<<"\""<<endl;
+	return 0;
+}
+
