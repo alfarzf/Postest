738b7de33ed6a36e68f766ab5a1446e58e90a848
Alfa Rizki Fadlillah <92286829+alfarzf@users.noreply.github.com>
2021-11-14 19:52:57 +0700

    Add files via upload

---
+++ 2117051003_Alfa Rizki Fadlillah_Postest2.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+	/*Alfa Rizki Fadlillah
+		2117051003
+			Post Test 2
+				*/
+	
+int main(){
+	float a, b, c, d;
+	
+	//input
+	cout<<"Harga Sebuah Pena\t: ";
+	cin>>a;
+	cout<<"Harga Sebuah Buku\t: ";
+	cin>>b;
+	cout<<"Harga Sebuah Penghapus\t: ";
+	cin>>c;
+	cout<<"Harga Sebuah Penggaris\t: ";
+	cin>>d;
+	
+	//output
+	cout
+	<<endl
+	<<"Harga 3 Buah Pena\t: Rp."<<a*3<<endl
+	<<"Harga 3 Buah Buku\t: Rp."<<b*3<<endl
+	<<"Harga 3 Buah Penghapus\t: Rp."<<c*3<<endl
+	<<"Harga 3 Buah Penggaris\t: Rp."<<d*3<<endl;
+	return 0;
+}
+
